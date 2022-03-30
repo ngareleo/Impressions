9efe5d139ab0a4a0779df0f0d83f6ddb774c033c
ngareleo <ngarimwenda@gmail.com>
2022-03-30 03:47:03 +0300

    Fixed users route

--- Microservices/Authentication Service/routes/index.js
+++ Microservices/Authentication Service/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json(
+    {
+      message: "Welcome user to the authentication service"
+    }
+  );
 });
 
 module.exports = router;
--- Microservices/Authentication Service/routes/users.js
+++ Microservices/Authentication Service/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  return res.json(
+    {
+      message: "Welcome to the users route"
+    }
+  );
 });
 
 module.exports = router;
