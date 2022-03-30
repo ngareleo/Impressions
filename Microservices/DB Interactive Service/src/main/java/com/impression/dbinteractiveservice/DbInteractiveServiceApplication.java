632b79d4bf3130fe8240ec68dd72b78ac99df2e2
ngareleo <ngarimwenda@gmail.com>
2022-03-30 13:58:47 +0300

    Springboot autogen files

---
+++ Microservices/DB Interactive Service/src/main/java/com/impression/dbinteractiveservice/DbInteractiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.impression.dbinteractiveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbInteractiveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbInteractiveServiceApplication.class, args);
+    }
+
+}
---
+++ Microservices/DB Interactive Service/src/test/java/com/impression/dbinteractiveservice/DbInteractiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impression.dbinteractiveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbInteractiveServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
